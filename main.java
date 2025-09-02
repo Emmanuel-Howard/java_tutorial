324019b5cd347650db4fdd03ac0fe5087bb67b9a
Emmanuel-Howard <emmanuelhoward3000@gmail.com>
2025-09-02 08:12:02 -0400

    Learning Print Statements & Data Types

---
+++ main.java
@@ -0,0 +1,58 @@
+public class Main {
+    public static void main(String[] args){
+
+// PRINT STATEMENTS
+/*
+        System.out.print("Hello World!\n");
+        System.out.print("I like pizza, it's good\n");
+        System.out.print("Especially peperoni"); 
+*/
+/*
+        // VARIABLES (Primitive & Reference)
+        // 2 Steps to create a variable
+        // 1. Declare the variable
+        // 2. Assign the variable a value
+        // Data types: int, double, char, String, boolean
+
+// Int
+        int age = 25;
+        int year = 2024;
+        System.out.println(age);
+        System.out.println("The year is " + year);
+// Double
+        double price = 19.99;
+        double gpa = 3.5;
+        double temp = -12.4;
+
+        System.out.println("$" + price);
+// Char
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        System.out.println(grade);
+        System.out.println(symbol);
+// Boolean
+        boolean isStudent = true;
+        boolean ishappy = false;
+
+        System.out.println(isStudent);
+        if(isStudent != true){
+            System.out.println("You are a student");
+        }
+        else{
+            System.out.println("You are not a student");
+        }
+
+// REFERENCE DATA TYPES
+// STRING
+        String name = "Emmanuel";
+        String food = "Apples";
+
+        System.out.print("Hello " + name);
+*/
+
+
+
+    }
+}
